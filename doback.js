f989d9db57670df9eb7ee5f63a680942819330b2
Vino-ys <yoonsun.yoonsunny@gmail.com>
2026-06-01 13:51:01 +0900

    연결

---
+++ doback.js
@@ -0,0 +1,22 @@
+const cntBox = document.getElementById("countTest2");
+const numbersHTML = "0123456789".split("").map(n => `<div>${n}</div>`).join("");
+
+function RollingNum(number) { 
+  cntBox.innerHTML = "";
+
+  number.split("").forEach(targetNum => {
+
+    const numSpan = document.createElement("span");
+    numSpan.className = "num";
+
+    numSpan.innerHTML = `<span class="numList">${numbersHTML}</span>`; //다시보기
+    cntBox.appendChild(numSpan);
+    numSpan.querySelector(".numList").style.transform = `translateY(${targetNum * -120}px)`;
+  });
+}
+
+document.getElementById("startBtn").addEventListener("click", () => {
+  RollingNum(String(Math.floor(Math.random() * 1000)).padStart(3, "0"));
+});
+
+RollingNum("000");
